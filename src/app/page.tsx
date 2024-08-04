71e76d619971c52dcd25e6cd7c4ad35747eeafe9
Ganesh Srambikal <ganesh.srambikal@gmail.com>
2024-08-04 18:20:18 +0530

    UI Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12 md:py-22 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               <div className="flex flex-col justify-center space-y-4">
@@ -26,8 +26,9 @@ export default function Home() {
                     AI Generated Content
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Create AI Generated descriptions, hashtags and outlines for your social media posts on YouTube, LinkedIn, Instagram, TikTok and X(formerly Twitter)
+                    Create AI Generated descriptions, hashtags and outlines for your social media posts on YouTube, LinkedIn, Instagram, TikTok and X(formerly Twitter).
                   </p>
+                  <p className="max-w-[600px] text-muted-foreground md:text-md font-light">Powered by <span className="text-blue-800 font-medium">Google Gemini</span></p>
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Link
@@ -125,6 +126,10 @@ export default function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">&copy; 2024 Content Crafter. All rights reserved.</p>
+        <div className="flex gap-3 ml-3 items-center">
+          <Image src='/razorpay.png' alt="razorpay" width={100} height={100} />
+          <Image src='/stripe.png' alt="stripe" width={60} height={40} />
+        </div>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
             Terms of Service
